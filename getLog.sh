be026092fdd73b9826e87bb6569cc9f02e81a334
Tody Kwok <tody.kwok@gmail.com>
2012-11-09 12:19:00 -0200

    fix &gt; to >

--- getLog.sh
+++ getLog.sh
@@ -15,16 +15,16 @@ echo Waiting for Android devices ready...
 adb wait-for-devices
 
 echo Dumping dmesg...
-adb shell dmesg &gt; dmesg.log
+adb shell dmesg > dmesg.log
 
 echo Dumping logcat...
-adb logcat -v time -d &gt;logcat.log
+adb logcat -v time -d >logcat.log
 
 echo Dumping radio...
-adb logcat -v time -d -b radio &gt;logcat_radio.log
+adb logcat -v time -d -b radio >logcat_radio.log
 
 echo Dumping events...
-adb logcat -v time -d -b events &gt;logcat_events.log
+adb logcat -v time -d -b events >logcat_events.log
 
 echo "Ziping files(logs_${defTime}.tar.gz)..."
 sleep 3
